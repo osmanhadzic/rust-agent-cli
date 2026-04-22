093ebd34723c576422df4deca987bbfa807568df
Osman Hadzic <osman.hadzic@secomind.com>
2026-04-22 08:56:58 +0200

    Initial commit

---
+++ target/debug/ai-agent-cli.d
@@ -0,0 +1,1 @@
+/home/osman/Documents/Osman/Rust/ai-agent-cli/target/debug/ai-agent-cli: /home/osman/Documents/Osman/Rust/ai-agent-cli/src/main.rs
---
+++ target/debug/deps/ai_agent_cli-fe1a7fa2f62a43e9.d
@@ -0,0 +1,5 @@
+/home/osman/Documents/Osman/Rust/ai-agent-cli/target/debug/deps/ai_agent_cli-fe1a7fa2f62a43e9.d: src/main.rs
+
+/home/osman/Documents/Osman/Rust/ai-agent-cli/target/debug/deps/ai_agent_cli-fe1a7fa2f62a43e9: src/main.rs
+
+src/main.rs:
